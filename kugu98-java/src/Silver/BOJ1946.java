d2b37ed6e4e75ccabc8a70571738a818a8fc073a
gu <82009882+kugu98@users.noreply.github.com>
2023-04-10 21:09:38 +0900

    BOJ1946

--- kugu98-java/src/Silver/BOJ1946.java
+++ kugu98-java/src/Silver/BOJ1946.java
@@ -1,9 +1,34 @@
 package Silver;
 
-import java.io.IOException;
+import java.io.*;
+import java.util.*;
 
 public class BOJ1946 {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        StringTokenizer st;
 
+        for (int i = 0; i < t; i++) {
+            int n = Integer.parseInt(br.readLine());
+            int[] rank = new int[n + 1];
+            for (int j = 0; j < n; j++) {
+                st = new StringTokenizer(br.readLine());
+                int a = Integer.parseInt(st.nextToken());
+                int b = Integer.parseInt(st.nextToken());
+                rank[a] = b;
+            }
+
+            int ans = 1;
+            int min = rank[1];
+            for (int j = 2; j <= n; j++) {
+                if (rank[j] < min) {
+                    ans++;
+                    min = rank[j];
+                }
+            }
+            System.out.println(ans);
+        }
     }
 }
+
